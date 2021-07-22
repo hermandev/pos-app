c90e92275f7cf591e4e489c6b737f7dc4d4b07b3
corn404 <codecorn404@gmail.com>
2021-07-22 22:48:42 +0800

    setup database

---
+++ src/main/resources/db/migration/V20210701__Schema_awal.sql
@@ -0,0 +1,21 @@
+create table m_barang(
+    id varchar(36) not null,
+    primary key(id)
+);
+
+create table m_kategori(
+    id varchar(36) not null,
+    primary key(id)
+);
+
+
+create table m_merek(
+    id varchar(36) not null,
+    primary key(id)
+);
+
+
+create table m_satuan(
+    id varchar(36) not null,
+    primary key(id)
+);
