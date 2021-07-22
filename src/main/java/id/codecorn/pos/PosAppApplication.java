ef4401091ad30ccb6ac52ed0cfd7e7ae04fa60af
corn404 <codecorn404@gmail.com>
2021-07-22 22:22:07 +0800

    starter project

---
+++ src/main/java/id/codecorn/pos/PosAppApplication.java
@@ -0,0 +1,13 @@
+package id.codecorn.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/codecorn/pos/PosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.codecorn.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
